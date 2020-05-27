efb31789318f0b31c611b9c8c856245569bcc40e
MamtaBobal <mamta.cs.du.2013@gmail.com>
2020-05-27 13:29:05 +0530

    if user is not authorized then error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  protect_from_forgery
+  before_action :authenticate_user!
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def user_not_authorized
+    flash[:notice] = "You are not authorized to perform this action"
+    respond_to do |format|
+      format.html do
+        redirect_to request.headers["Referer"] || root_path
+      end
+    end
+  end
+
 end
