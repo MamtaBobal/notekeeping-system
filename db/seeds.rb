f594bca8cbde8c2dcc2a4b0d14b5a8da35d4114d
MamtaBobal <mamta.cs.du.2013@gmail.com>
2020-05-29 01:11:25 +0530

    Add seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+600.times do |i|
+  User.create(
+               email: Faker::Internet.email,
+               password: 123456
+             )
+end
+
+3000.times do |i|
+  Note.create(
+                description: Faker::Movies::StarWars.quote
+              )
+end
+
+3000.times do |i|
+  begin UserNote.create(user_id: rand(1..200), note_id: rand(1..100), role: UserNote::roles.invert[0])
+  rescue => error
+    next
+  end
+end
